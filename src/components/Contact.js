2068f63ad625fd4490a21b7ba67435b12cce66b9
Tej-Deep <ptejdeep@gmail.com>
2023-10-01 14:30:00 +0800

    fixing issue of form submission

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -69,8 +69,10 @@ export default function Contact() {
         <form
           netlify
           name="contact"
+          method = "post"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <input type="hidden" name="form-name" value="contact" />
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Contact Me
           </h2>
